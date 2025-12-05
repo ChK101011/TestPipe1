1e952234070b8bb13b6d322338a57134653b3603
ChK101011 <chkthegeek@gmail.com>
2025-12-05 10:06:35 +0100

    Mise en place des tests

--- src/test/java/ma/ensa/calculatrice/CalculatriceApplicationTests.java
+++ src/test/java/ma/ensa/calculatrice/CalculatriceApplicationTests.java
@@ -1,5 +1,8 @@
 package ma.ensa.calculatrice;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +12,32 @@ class CalculatriceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	main calc = new main();
+
+    @Test
+    void testAdd() {
+        assertEquals(9, calc.add(4, 5));
+    }
+
+    @Test
+    void testSub() {
+        assertEquals(1, calc.sub(6, 5));
+    }
+
+    @Test
+    void testMul() {
+        assertEquals(24, calc.mul(4, 6));
+    }
+
+    @Test
+    void testDiv() {
+        assertEquals(5, calc.div(10, 2));
+    }
+
+    @Test
+    void testDivByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calc.div(10, 0));
+    }
 
 }
