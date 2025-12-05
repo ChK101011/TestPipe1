46984e78471489f114afb7b0c6743d0fa8288680
ChK101011 <chkthegeek@gmail.com>
2025-12-05 09:23:55 +0100

    Premier commit

---
+++ src/main/java/ma/ensa/calculatrice/main.java
@@ -0,0 +1,23 @@
+package ma.ensa.calculatrice;
+
+public class main {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double sub(double a, double b) {
+        return a - b;
+    }
+
+    public double mul(double a, double b) {
+        return a * b;
+    }
+
+    public double div(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division par zéro impossible");
+        }
+        return a / b;
+    }
+}
